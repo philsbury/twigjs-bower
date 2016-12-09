106743ddc596d6ad9371e8f4f14a206f5e592596
Phil Baker <phil@philsbury.co.uk>
2016-12-09 12:38:44 +0000

    Added task to generate twig js files

---
+++ Gruntfile.js
@@ -0,0 +1,44 @@
+module.exports = function(grunt) {
+
+  var pkg = grunt.file.readJSON('package.json');
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: pkg,
+    exec: {
+      update: "cd ./twig.js/ git pull origin master && cd ../",
+      build: "cd ./twig.js/ && npm run build && cd ../"
+    },
+    copy: {
+      main: {
+        files: [
+          {
+            cwd: 'twig.js/',
+            src: 'twig.*',
+            dest: 'twig/',
+            expand: true
+          }
+        ]
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  require('load-grunt-tasks')(grunt);
+
+  grunt.registerTask('test', function() {
+    var twig = grunt.file.readJSON('twig.js/package.json');
+
+    if(pkg.version !== twig.version){
+      pkg.version = twig.version;
+      var fs = require("fs"); 
+      var json = JSON.stringify(pkg, null, 4); 
+      fs.writeFileSync("package.json",json);
+    }
+
+  });
+
+  // Default task(s).
+  grunt.registerTask('default', ['exec:update', 'exec:build', 'copy', 'test']);
+
+};
